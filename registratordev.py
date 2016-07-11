0cbbf74839657a6d8cefa47ed336fad40b4a9f2e
Arti Markelov <yardalgedal@gmail.com>
2016-07-11 22:12:50 +0300

    Update registratordev.py

--- registratordev.py
+++ registratordev.py
@@ -69,7 +69,7 @@ root.maxsize(width=1000,height=400)
  
 def buttonreg(event):
     pool = ThreadPool(int(cthreads.get()))
-    pool.map(nf, proxylist)
+    pool.map_async(nf, proxylist)
     pool.close()
     pool.join()
     ctypes.windll.user32.MessageBoxW(None,"Выполнение завершено","Регистратор",0x40 | 0x0)
