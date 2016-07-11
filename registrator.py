f9a2975d58b0965efc83569b914198ac990d3e9b
Arti Markelov <yardalgedal@gmail.com>
2016-07-11 10:48:24 +0300

    Update registrator.py

--- registrator.py
+++ registrator.py
@@ -43,7 +43,7 @@ def nf(proxy):
             pass
         
         r = register(available_r,login,password,{'https':proxy.rstrip()})
-        print('Account registration attemp ' + login + ':' + password + ' with proxy ' + proxy.rstrip() + '\n available: ' + available_r.text + '\n response: ' + r.text + '\n')
+        print('Account registration attempt ' + login + ':' + password + ' with proxy ' + proxy.rstrip() + '\n available: ' + json.loads(available_r.text) + '\n response: ' + json.loads(r.text) + '\n')
         jsonlr = json.loads(r.text)
         invalid_txt.insert(END, proxy + ' | '+ str(jsonlr))
         if jsonlr['account_created'] == True:
