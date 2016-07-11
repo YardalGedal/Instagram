d556532c48cb1f79aa0fb7349b3fc67c47e1ed62
Arti Markelov <yardalgedal@gmail.com>
2016-07-11 22:54:12 +0300

    Update registratordev.py

--- registratordev.py
+++ registratordev.py
@@ -69,7 +69,7 @@ root.maxsize(width=1000,height=400)
  
 def buttonreg(event):
     pool = ThreadPool(int(cthreads.get()))
-    pool.apply_async(nf, proxylist, callback=ctypes.windll.user32.MessageBoxW(None,"Выполнение завершено","Регистратор",0x40 | 0x0))
+    pool.map_async(nf, proxylist, callback=ctypes.windll.user32.MessageBoxW(None,"Выполнение завершено","Регистратор",0x40 | 0x0))
     pool.close()
 
 invalid_txt = Listbox(root, bg="white", fg="black")
